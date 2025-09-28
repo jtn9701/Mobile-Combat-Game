024a7a84882919e6d8fbcba4a8339f747c266239
jtn9701 <jtnguy01@gmail.com>
2025-09-28 15:06:47 -0500

    Modify styles of main menu, credits, and how to play

--- mobile-combat-game/components/InformationModal.js
+++ mobile-combat-game/components/InformationModal.js
@@ -1,19 +1,20 @@
-import { View, FlatList, Button, Text } from 'react-native';
+import { View, FlatList, Button, Text, Image } from 'react-native';
 
 import { globalStyles } from './style-sheets/global-styles';
 import { screenStyles } from './style-sheets/screen-styles';
 
 const InformationModal = ({data, screenValueSetter}) => {
     const renderModalItem = ({item}) => {
-        return <Text style={[screenStyles.mainMenuStyles.navButtons, globalStyles.borderForTesting, screenStyles.creditsStyles.textFont]}>{item.title}</Text>
+        return <Text style={[globalStyles.borderForTesting, screenStyles.creditsStyles.textFont, {alignSelf: 'center', textAlign: 'center', width: '80%', paddingVertical: 10}]}>{item.title}</Text>
     }
 
     return (
         <View style={[globalStyles.container, globalStyles.background, screenStyles.mainMenuStyles.backgroundImage]}>
             <View style={[globalStyles.container, globalStyles.borderForTesting, screenStyles.creditsStyles.modal]}>
-                <FlatList data={data} renderItem={renderModalItem}/>
+                <Image style={{height: 200, width: 200, marginBottom: 10}} source={require('./../assets/images/info.png')}/>
+                <FlatList style={{width: '90%'}} data={data} renderItem={renderModalItem}/>
+                <Button title='Exit' onPress={() => {screenValueSetter(0)}}/>
             </View>
-            <Button title='Exit' onPress={() => {screenValueSetter(0)}}/>
         </View>
     )
 }
